d53200e32ee5aac20ff18938843c7d0547478554
Alex Schütz <info@alexschuetz.de>
2022-03-24 22:17:38 +0100

    added mac tools

---
+++ play_mac.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/Applications/RetroArch.app/Contents/MacOS/RetroArch -L "/Applications/RetroArch.app/Contents/Resources/cores/genesis_plus_gx_libretro.dylib" -c "/Users/ArcadeTV/Library/Application Support/RetroArch/config/retroarch.cfg" "./roms/Wonder Boy in Monster World (T-German).md"
