2ef7184f1fa4651f272e96e5adefda153ab17a40
ArcadeTV <info@arcade-tv.de>
2022-03-27 21:24:14 +0200

    filesystem cleanup

--- build.bat
+++ build.bat
@@ -9,7 +9,7 @@ set VERSION=%CUR_YYYY%%CUR_MM%%CUR_DD%-%CUR_HH%%CUR_NN%%CUR_SS%
 echo     dc.b " - %date% - ">.\includes\version.asm
 
 echo Building...
-.\tools\vasmm68k_mot_win32.exe .\patch_de.asm -quiet -chklabels -nocase -rangewarnings -Dvasm=1 -L tmp\Listing.txt -DBuildGEN=1 -Fbin -Fsrec -o tmp\srecfile.txt
-.\tools\srecpatch.exe ".\roms\Wonder Boy in Monster World (USA, Europe).md" ".\roms\Wonder Boy in Monster World (T-German).md"<.\tmp\srecfile.txt >.\tmp\srecpatch.txt
-.\tools\fixheader.exe ".\roms\Wonder Boy in Monster World (T-German).md"
+.\tools\win\vasmm68k_mot_win32.exe .\patch_de.asm -quiet -chklabels -nocase -rangewarnings -Dvasm=1 -L tmp\Listing.txt -DBuildGEN=1 -Fbin -Fsrec -o tmp\srecfile.txt
+.\tools\win\srecpatch.exe ".\roms\Wonder Boy in Monster World (USA, Europe).md" ".\roms\Wonder Boy in Monster World (T-German).md"<.\tmp\srecfile.txt >.\tmp\srecpatch.txt
+.\tools\win\fixheader.exe ".\roms\Wonder Boy in Monster World (T-German).md"
 echo done!
--- fix_permissions.sh
+++ fix_permissions.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 chmod +x build.sh
-chmod +x tools_mac/fixheader
-chmod +x tools_mac/srecpatch
-chmod +x tools_mac/vasmm68k_mot
+chmod +x tools/mac/fixheader
+chmod +x tools/mac/srecpatch
+chmod +x tools/mac/vasmm68k_mot
 chmod +x play_mac.sh
